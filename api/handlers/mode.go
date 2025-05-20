95f74d4bb4f190aaeffdeef8e46610e20373580a
helican <james-knippes@mailbox.org>
2025-05-20 19:29:14 +0200

    feat: add logic to overwrite current mode via env variable

--- api/handlers/mode.go
+++ api/handlers/mode.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"math/rand"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,7 +17,18 @@ import (
 // @Router /mode [get]
 func Mode(c *gin.Context) {
 	// random number for demonstration purposes 1-3
-	mode := rand.Intn(3)
+	var mode int
+	value := os.Getenv("KB_MODE_OVERRIDE")
+	if value == "" {
+		mode = rand.Intn(3)
+	} else {
+		converted, err := strconv.Atoi(value)
+		if err == nil {
+			mode = converted
+		} else {
+			mode = rand.Intn(3)
+		}
+	}
 	c.JSON(http.StatusOK, gin.H{
 		"mode": mode, // e.g., "normal", "maintenance"
 	})
