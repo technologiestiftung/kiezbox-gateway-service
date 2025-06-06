b87c08d4c4926bb1ae7fb298769dfea6f7f609c6
helican <james-knippes@mailbox.org>
2025-06-06 20:02:16 +0200

    feat: move bulk of the config from main to config package

--- cmd/main.go
+++ cmd/main.go
@@ -2,34 +2,14 @@ package main
 
 import (
 	"context"
-	"kiezbox/internal/config"
+	c "kiezbox/internal/config"
 	"kiezbox/internal/db"
 	"kiezbox/internal/meshtastic"
 	"kiezbox/logging"
-	"log"
 	"log/slog"
 	"sync"
 	"time"
-
-	"github.com/BoRuDar/configuration/v4"
-	"github.com/tarm/serial"
 )
-
-type Config struct {
-    SetTime bool `flag:"settime" default:"true"`
-    DbWriter bool `flag:"dbwriter" default:"true"`
-    DbRetry bool `flag:"dbretry" default:"true"`
-    SerialDevice string `flag:"serial_dev" default:"/dev/ttyUSB0"`
-    SerialBaud int `flag:"serial_baud" default:"115200"`
-    RetryInterval int `flag:"retry_interval" default:"60"`
-    CacheDir string `flag:"cache_dir" default:".kb-dbcache"`
-    DbTimeout int `flag:"db_timeout" default:"5"`
-    ApiPort string `flag:"api_port" default:"9080"`
-    SessionDir string `flag:"api_sessiondir" default:".kb-session"`
-}
-
-// Global gateway service config
-var gwConfig Config
 
 // Using an interface as an intermediate layer instead of calling the meshtastic functions directly
 // allows for dependency injection, essential for unittesting.
@@ -47,7 +27,7 @@ type MeshtasticDevice interface {
 }
 
 // RunGoroutines orchestrates the goroutines that run the service.
-func RunGoroutines(ctx context.Context, wg *sync.WaitGroup, device MeshtasticDevice, setTime bool, dbwriter bool, dbretry bool, db_client *db.InfluxDB) {
+func RunGoroutines(ctx context.Context, wg *sync.WaitGroup, device MeshtasticDevice, db_client *db.InfluxDB) {
 	// Launch goroutines
 	wg.Add(1)
 	go device.Writer(ctx, wg)
@@ -62,14 +42,14 @@ func RunGoroutines(ctx context.Context, wg *sync.WaitGroup, device MeshtasticDev
 	wg.Add(1)
 	go device.ConfigWriter(ctx, wg)
 
-	if setTime {
+	if c.Cfg.SetTime {
 		// We wait for the not info to set the time
 		wg.Add(1)
 		go device.Settime(ctx, wg, time.Now().Unix())
 	}
 
 	// Process incoming KiexBox messages in its own goroutine
-	if dbwriter {
+	if c.Cfg.DbWriter {
 		wg.Add(1)
 		go device.DBWriter(ctx, wg, db_client)
 		// } else {
@@ -77,7 +57,7 @@ func RunGoroutines(ctx context.Context, wg *sync.WaitGroup, device MeshtasticDev
 	}
 
 	// Start the retry mechanism in its own goroutine
-	if dbretry {
+	if c.Cfg.DbRetry {
 		wg.Add(1)
 		go device.DBRetry(ctx, wg, db_client)
 	}
@@ -92,40 +72,26 @@ func main() {
 	// 1. cli argurments
 	// 2. environment variables
 	// 3. default values
-	configurator := configuration.New(
-		&gwConfig,
-		configuration.NewFlagProvider(),
-		configuration.NewEnvProvider(),
-		configuration.NewDefaultProvider(),
-	)
-	if err := configurator.InitValues(); err != nil {
-		log.Fatal("Configuration error: ", err)
-	}
+	c.LoadConfig()
 	logging.InitLogger(logging.LoggerConfig{
-		Level:     slog.LevelInfo,
+		Level:     slog.Level(c.Cfg.LogLevel),
 		Format:    "text",
-		Filename:  "",
-		AddSource: true,
-		ShortPath: true,
+		LogFile:  c.Cfg.LogFile,
+		LogToFile:  c.Cfg.LogToFile,
+		AddSource: c.Cfg.LogSource,
+		ShortPath: c.Cfg.LogShortPath,
 	})
 
 	slog.Info("Logger initialized", "app", "kiezbox-gateway-service")
-	slog.Debug("Service configuration", "cfg", gwConfig)
+	slog.Debug("Service configuration", "cfg", c.Cfg)
 
 	// Initialize meshtastic serial connection
 	var mts meshtastic.MTSerial
 
-	// TODO: portFactory is also defined in serial.go, this should be taken care of eventally
-	portFactory := func(conf *serial.Config) (meshtastic.SerialPort, error) {
-		return serial.OpenPort(conf)
-	}
-	mts.Init(gwConfig.SerialDevice, gwConfig.SerialBaud, gwConfig.RetryInterval, gwConfig.ApiPort, portFactory, gwConfig.CacheDir, gwConfig.SessionDir)
-
-	// Load InfluxDB configuration
-	url, token, org, bucket := config.LoadConfig()
+	mts.Init(c.Cfg.SerialDevice, c.Cfg.SerialBaud, c.Cfg.RetryInterval, c.Cfg.ApiPort, meshtastic.CreateSerialPort, c.Cfg.CacheDir, c.Cfg.SessionDir)
 
 	// Initialize InfluxDB client
-	db_client := db.CreateClient(url, token, org, bucket, gwConfig.DbTimeout)
+	db_client := db.CreateClient()
 	defer db_client.Close()
 
 	// Create a context with cancel
@@ -136,7 +102,7 @@ func main() {
 	var wg sync.WaitGroup
 
 	// Run the goroutines
-	RunGoroutines(ctx, &wg, &mts, gwConfig.SetTime, gwConfig.DbWriter, gwConfig.DbRetry, db_client)
+	RunGoroutines(ctx, &wg, &mts, db_client)
 
 	// Wait for all goroutines to finish
 	wg.Wait()
--- internal/config/config.go
+++ internal/config/config.go
@@ -3,27 +3,71 @@ package config
 import (
 	"log"
 	"os"
+	"sync"
+	"time"
 
+	"github.com/BoRuDar/configuration/v4"
 	"github.com/joho/godotenv"
 )
 
-func LoadConfig() (string, string, string, string) {
-	// Load environment variables from .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
+type GatewayConfig struct {
+    SetTime bool `flag:"settime" default:"true"`
+    DbWriter bool `flag:"dbwriter" default:"true"`
+    DbRetry bool `flag:"dbretry" default:"true"`
+    DbUrl string `flag:"dburl" env:"INFLUXDB_URL"`
+    DbToken string `flag:"dbtoken" env:"INFLUXDB_TOKEN"`
+    DbOrg string `flag:"dborg" env:"INFLUXDB_ORG"`
+    DbBucket string `flag:"dbbucket" env:"INFLUXDB_BUCKET"`
+    SerialDevice string `flag:"serial_dev" default:"/dev/ttyUSB0"`
+    SerialBaud int `flag:"serial_baud" default:"115200"`
+    RetryInterval int `flag:"retry_interval" default:"60"`
+    CacheDir string `flag:"cache_dir" default:".kb-dbcache"`
+    DbTimeout time.Duration `flag:"db_timeout" default:"5s"`
+    ApiPort string `flag:"api_port" default:"9080"`
+    SessionDir string `flag:"api_sessiondir" default:".kb-session"`
+    LogLevel int `flag:"log_level" default:"0"` //slog logging levels constants are defined here. 0 is LevelInfo > https://pkg.go.dev/log/slog#LevelInfo
+    LogFile string `flag:"log_file" default:".kb-gwlog"`
+    LogToFile bool `flag:"log_tofile" default:"false"`
+    LogSource bool `flag:"log_source" default:"true"`
+    LogShortPath bool `flag:"log_shortpath" default:"true"`
+}
+// Global gateway service config
+var Cfg GatewayConfig
 
-	// Fetch InfluxDB configuration from environment variables
-	url := os.Getenv("INFLUXDB_URL")
-	token := os.Getenv("INFLUXDB_TOKEN")
-	org := os.Getenv("INFLUXDB_ORG")
-	bucket := os.Getenv("INFLUXDB_BUCKET")
+// Make sure that the config is only loaded once
+var once sync.Once
 
-	// Check if any required variable is missing
-	if url == "" || token == "" || org == "" || bucket == "" {
-		log.Fatal("Missing one or more required environment variables")
-	}
+func LoadConfig() {
+	loadConfig(false)
+}
 
-	return url, token, org, bucket
+func LoadConfigNoFail() {
+	loadConfig(true)
+}
+
+func loadConfig(nofail bool) {
+	once.Do(func() {
+		// Also load environment variables from .env file
+		cwd, _ := os.Getwd()
+		err := godotenv.Load()
+		if err != nil {
+			log.Printf("Failed loading %s.env file: %v", cwd, err)
+		}
+		configurator := configuration.New(
+			&Cfg,
+			configuration.NewFlagProvider(),
+			configuration.NewEnvProvider(),
+			configuration.NewDefaultProvider(),
+		)
+		if nofail {
+			configurator.SetOptions(
+				configuration.OnFailFnOpt(func(err error) {
+					log.Println(err)
+				}),
+			)
+		}
+		if err := configurator.InitValues(); err != nil {
+			log.Fatal("Configuration error: ", err)
+		}
+	})
 }
