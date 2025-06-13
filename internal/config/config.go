bc7d506ddfe91a1e997d94b9c655991d1252f964
helican <james-knippes@mailbox.org>
2025-06-13 15:51:26 +0200

    fix: add flag descriptions, that went missing during refactor

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,31 +11,31 @@ import (
 )
 
 type GatewayConfig struct {
-	SetTime       bool          `flag:"settime" default:"true"`
-	DbWriter      bool          `flag:"dbwriter" default:"true"`
-	DbRetry       bool          `flag:"dbretry" default:"true"`
-	DbUrl         string        `flag:"dburl" env:"INFLUXDB_URL"`
-	DbToken       string        `flag:"dbtoken" env:"INFLUXDB_TOKEN"`
-	DbOrg         string        `flag:"dborg" env:"INFLUXDB_ORG"`
-	DbBucket      string        `flag:"dbbucket" env:"INFLUXDB_BUCKET"`
-	SerialDevice  string        `flag:"serial_dev" default:"/dev/ttyUSB0"`
-	SerialBaud    int           `flag:"serial_baud" default:"115200"`
-	RetryInterval time.Duration `flag:"retry_interval" default:"60s"`
-	CacheDir      string        `flag:"cache_dir" default:".kb-dbcache"`
-	DbTimeout     time.Duration `flag:"db_timeout" default:"5s"`
-	ApiPort       string        `flag:"api_port" default:"9080"`
-	SessionDir    string        `flag:"api_sessiondir" default:".kb-session"`
-	LogLevel      int           `flag:"log_level" default:"0"` //slog logging levels constants are defined here. 0 is LevelInfo > https://pkg.go.dev/log/slog#LevelInfo
-	LogFile       string        `flag:"log_file" default:".kb-gwlog"`
-	LogToFile     bool          `flag:"log_tofile" default:"false"`
-	LogSource     bool          `flag:"log_source" default:"true"`
-	LogShortPath  bool          `flag:"log_shortpath" default:"true"`
-	SipTrunkBase  string        `uci:"trunk_base" env:"SIP_TRUNK_BASE" default:"2"`
-	BoxLat        float32       `uci:"geo_lat" env:"BOX_LAT" default:"0"`
-	BoxLon        float32       `uci:"geo_lon" env:"BOX_LON" default:"0"`
-	Mode          int           `uci:"mode" env:"KB_MODE" default:"0"`
-	ModeOverride  bool          `uci:"mode_override" env:"KB_MODE_OVERRIDE" default:"false"`
-	CorsLocalhost bool          `uci:"cors_localhost" env:"CORS_LOCALHOST" default:"false"`
+	SetTime       bool          `flag:"settime|true|Sets the RTC time of the device to the system time at service startup" default:"true"`
+	DbWriter      bool          `flag:"dbwriter|true|Enables the dbwriter routine, which forwards sensor datapoints from meshtastic to the influxdb" default:"true"`
+	DbRetry       bool          `flag:"dbretry|true|Enables the dbretry routine, which preiodically moves cached datapoints into the influxdb" default:"true"`
+	DbUrl         string        `flag:"dburl||Full URL of the influxdb" env:"INFLUXDB_URL"`
+	DbToken       string        `flag:"dbtoken||API token for the influxdb" env:"INFLUXDB_TOKEN"`
+	DbOrg         string        `flag:"dborg||Organisation to use in the influxdb" env:"INFLUXDB_ORG"`
+	DbBucket      string        `flag:"dbbucket||Bucket to use in the influxdb" env:"INFLUXDB_BUCKET"`
+	SerialDevice  string        `flag:"serial_dev|/dev/ttyUSB0|The serial device connected to the meshtastic device" default:"/dev/ttyUSB0"`
+	SerialBaud    int           `flag:"serial_baud|115200|Baud rate of the serial device" default:"115200"`
+	RetryInterval time.Duration `flag:"retry_interval|60s|Time interval (as time.Duration) for the dbretry delay" default:"60s"`
+	CacheDir      string        `flag:"cache_dir|.kb-dbcache|Directory for caching datapoints" default:".kb-dbcache"`
+	DbTimeout     time.Duration `flag:"db_timeout|5s|Database timeout (as time.Duration)" default:"5s"`
+	ApiPort       string        `flag:"api_port|9080|Port to use for the gateway service HTTP API" default:"9080"`
+	SessionDir    string        `flag:"api_sessiondir|.kb-session|Directory for storing emergency call user sessions" default:".kb-session"`
+	LogLevel      int           `flag:"log_level|0|Loglevel (int) as defined by go slog" default:"0"` //slog logging levels constants are defined here. 0 is LevelInfo > https://pkg.go.dev/log/slog#LevelInfo
+	LogFile       string        `flag:"log_file|.kb-gwlog|Log file for slog" default:".kb-gwlog"`
+	LogToFile     bool          `flag:"log_tofile|false|Enables logging to the logfile instead of standard output" default:"false"`
+	LogSource     bool          `flag:"log_source|true|Enables logging the filename with slog" default:"true"`
+	LogShortPath  bool          `flag:"log_shortpath|true|Enables short filename format (basename only) for slog" default:"true"`
+	SipTrunkBase  string        `uci:"trunk_base|2|Sup trunk base phone number" env:"SIP_TRUNK_BASE" default:"2"`
+	BoxLat        float32       `uci:"geo_lat|0|Geolocation latitude of the box" env:"BOX_LAT" default:"0"`
+	BoxLon        float32       `uci:"geo_lon|0|Geolocation longitude of the box" env:"BOX_LON" default:"0"`
+	Mode          int           `uci:"mode|0|Default device mode as defined in the protobuf" env:"KB_MODE" default:"0"`
+	ModeOverride  bool          `uci:"mode_override|false|Enables overwriting the device mode with the cli/env flag" env:"KB_MODE_OVERRIDE" default:"false"`
+	CorsLocalhost bool          `uci:"cors_localhost|false|Adds CORS header for local testing only" env:"CORS_LOCALHOST" default:"false"`
 }
 
 // Global gateway service config
