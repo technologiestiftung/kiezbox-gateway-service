373abc4c0605338e60855e2c507db7739570a008
guadiromero <guadi1994@gmail.com>
2025-06-17 15:52:19 +0200

    feat: add state mode setter and getter with thread safety

--- internal/state/state.go
+++ internal/state/state.go
@@ -1,12 +1,28 @@
-package config
+package state
 
 import (
 	"kiezbox/internal/github.com/meshtastic/go/generated"
+	"sync"
 )
 
 type GatewayState struct {
-	Mode generated.KiezboxMessage_Mode
+	mutex sync.RWMutex
+	mode  generated.KiezboxMessage_Mode
 }
 
 // Global gateway service config
 var State GatewayState
+
+// SetMode safely sets the Mode field of the global State
+func SetMode(newMode generated.KiezboxMessage_Mode) {
+	State.mutex.Lock()
+	defer State.mutex.Unlock()
+	State.mode = newMode
+}
+
+// GetMode safely gets the Mode field of the global State
+func GetMode() generated.KiezboxMessage_Mode {
+	State.mutex.RLock()
+	defer State.mutex.RUnlock()
+	return State.mode
+}
